c5b7794bfd861b31265675254d0cb4d228855ddc
Andrey Romanov <steampart@gmail.com>
2021-03-19 13:47:37 +0300

    formatting

--- redis.go
+++ redis.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"runtime"
-	"fmt"
-	"time"
-	"context"
-	"strings"
-	"strconv"
+    "runtime"
+    "fmt"
+    "time"
+    "context"
+    "strings"
+    "strconv"
     "github.com/go-redis/redis/v8"
 )
 
@@ -18,18 +18,18 @@ func insertRedisData(itemsNum int) {
 
 	var ctx = context.Background()
 
-    rdb := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    })
+        rdb := redis.NewClient(&redis.Options{
+           Addr:     "localhost:6379",
+           Password: "", // no password set
+           DB:       0,  // use default DB
+       })
 
 	start := time.Now()
 	for i := 0; i < itemsNum; i++ {
 		item := newItem(i)
 
-        var sb1 strings.Builder
-        var sb2 strings.Builder
+                var sb1 strings.Builder
+                var sb2 strings.Builder
 
 		sb1.WriteString(strconv.Itoa(int(item.ID)))
 
@@ -41,18 +41,18 @@ func insertRedisData(itemsNum int) {
 
 
 		err := rdb.Set(ctx, sb1.String(), sb2.String(), 0).Err()
-        if err != nil {
-            panic(err)
-        }
+                if err != nil {
+                      panic(err)
+                }
 
 	}
 
 	var m runtime.MemStats
-    runtime.ReadMemStats(&m)
-    fmt.Printf("Alloc = %v MiB", bRedisToMb(m.Alloc))
-    fmt.Printf("\tTotalAlloc = %v MiB", bRedisToMb(m.TotalAlloc))
-    fmt.Printf("\tSys = %v MiB", bRedisToMb(m.Sys))
-    fmt.Printf("\tNumGC = %v\n", m.NumGC)
+        runtime.ReadMemStats(&m)
+        fmt.Printf("Alloc = %v MiB", bRedisToMb(m.Alloc))
+        fmt.Printf("\tTotalAlloc = %v MiB", bRedisToMb(m.TotalAlloc))
+        fmt.Printf("\tSys = %v MiB", bRedisToMb(m.Sys))
+        fmt.Printf("\tNumGC = %v\n", m.NumGC)
 
 	fmt.Println(time.Since(start))
 
